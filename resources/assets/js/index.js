33999b7888dffaa83a0f8c89fa2047fb415e6dee
Waylon <wdixon@waylondixon.com>
2017-11-09 19:15:13 -0700

    just saving

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -30,7 +30,7 @@ const store = createStore(reducer);
 
 firebaseApp.auth().onAuthStateChanged((user) => {
    if(user) {
-       //console.log('user has signed in or up', user);
+       //console.log('user has signed  in or up', user);
 
        const { email } = user;
        store.dispatch(logUser(email));
